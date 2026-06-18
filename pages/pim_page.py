93ae97e61182c9f1544db1d6480f2255bf78895c
Eva Nur Azizah Indah <evaannrr@gmail.com>
2026-06-18 10:17:31 +0700

    Adding search valid employee id test case

--- pages/pim_page.py
+++ pages/pim_page.py
@@ -15,8 +15,17 @@ class PimPage:
     def search_employee_by_name(self, employee_name):
         self.page.get_by_role("textbox", name="Type for hints...").first.fill(employee_name)
         self.page.get_by_role("button", name="Search").click()
+
+    def search_employee_by_id(self, employee_id):
+        self.page.get_by_role("textbox").nth(2).fill(employee_id)
+        self.page.get_by_role("button", name="Search").click()
     
     def is_employee_name_displayed(self, employee_name):
         locator = self.page.get_by_text(employee_name)
+        locator.wait_for(timeout=10000)
+        return locator.is_visible()
+
+    def is_employee_id_displayed(self, employee_id):
+        locator = self.page.get_by_text(employee_id)
         locator.wait_for(timeout=10000)
         return locator.is_visible()
---
+++ tests/PIM_search_valid_employee_id_test.py
@@ -0,0 +1,26 @@
+from pages.login_page import LoginPage
+from pages.pim_page import PimPage
+
+def test_search_valid_employee_id(page):
+    login = LoginPage(page)
+    pim = PimPage(page)
+
+    employee_id = "0001"
+
+    login.open()
+    login.login("Admin", "admin123")
+
+    page.wait_for_url("**dashboard**", timeout=15000)
+    page.get_by_role("heading", name="Dashboard").wait_for(timeout=15000)
+
+    pim.open_pim()
+    pim.open_employee_list()
+    pim.search_employee_by_id(employee_id)
+    assert pim.is_employee_id_displayed(employee_id)
+
+
+
+    
+    
+
+    
